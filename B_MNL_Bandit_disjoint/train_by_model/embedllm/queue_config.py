d8ad4efce841bab3ac3f47a4b981a06c16396f7e
Junyoung0426 <sjy990426@gmail.com>
2026-01-22 23:41:35 +0900

    Changes to be committed: 	modified:   B_MNL_Bandit_disjoint/train_by_model/embedllm/queue_config.py

--- B_MNL_Bandit_disjoint/train_by_model/embedllm/queue_config.py
+++ B_MNL_Bandit_disjoint/train_by_model/embedllm/queue_config.py
@@ -44,7 +44,7 @@ class QueueConfig:
 
     # unknown horizon schedule
     kappa: float =4.0
-    c1: float = 40.0
+    c1: float = 6.0
 
     # ridge for theta + initial V scale (V = lambda_0 I + Σ z z^T)
     lambda_0: float = 1.0
