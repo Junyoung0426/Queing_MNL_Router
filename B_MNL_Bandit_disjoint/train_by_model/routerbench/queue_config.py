2ec30e86ec3a9c72f62fab4411d77b65e94f60f7
Junyoung0426 <sjy990426@gmail.com>
2026-01-22 15:02:23 +0900

    Changes to be committed: 	modified:   B_MNL_Bandit_disjoint/base_line/3qucb/embedllm_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/3qucb/mixinstruct_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/3qucb/routerbench_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/3qucb/sprout_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/4qths/embedllm_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/4qths/mixinstruct_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/4qths/routerbench_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/4qths/sprout_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/5cqb_epsilon/embedllm_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/5cqb_epsilon/mixinstruct_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/5cqb_epsilon/routerbench_train.py 	modified:   B_MNL_Bandit_disjoint/base_line/5cqb_epsilon/sprout_train.py 	modified:   B_MNL_Bandit_disjoint/train_by_model/embedllm/queue_config.py 	renamed:    B_MNL_Bandit_disjoint/train_by_model/mixinst/mixinstruct_train.py -> B_MNL_Bandit_disjoint/train_by_model/mixinstruct/mixinstruct_train.py 	renamed:    B_MNL_Bandit_disjoint/train_by_model/mixinst/queue_config.py -> B_MNL_Bandit_disjoint/train_by_model/mixinstruct/queue_config.py 	renamed:    B_MNL_Bandit_disjoint/train_by_model/router/queue_config.py -> B_MNL_Bandit_disjoint/train_by_model/routerbench/queue_config.py 	renamed:    B_MNL_Bandit_disjoint/train_by_model/router/routerbench_train.py -> B_MNL_Bandit_disjoint/train_by_model/routerbench/routerbench_train.py
    	modified:   B_MNL_Bandit_disjoint/B_tester.ipynb
    	modified:   B_MNL_Bandit_disjoint/base_line/2random_policy/routerbench_train.py
    	modified:   B_MNL_Bandit_disjoint/base_line/2random_policy/sprout_train.py

--- B_MNL_Bandit_disjoint/train_by_model/routerbench/queue_config.py
+++ B_MNL_Bandit_disjoint/train_by_model/routerbench/queue_config.py
@@ -47,7 +47,11 @@ class QueueConfig:
 
     # ridge for theta + initial V scale (V = lambda_0 I + Σ z z^T)
     lambda_0: float = 1.0
+<<<<<<< HEAD
     alpha_coef: float = 0.0001
+=======
+    alpha_coef: float = 0.001
+>>>>>>> c3f2a41 (Changes to be committed:)
 
     # theta solver (full-history)
     theta_solver: str = "lbfgs"  #  "lbfgs"
